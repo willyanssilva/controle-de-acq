a303695a1df09955b1c33f9354d9e279f2f40851
willyanssilva <willyansdare50@gmail.com>
2025-07-08 20:51:22 -0300

    atualização do input e select

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwScHu4cUVNVFnsYR7wzY_oh8EDB8xcjZ3ZH1uJqLzsFESFdEam_gu2VAHmEZCci-YX/exec'
-
-const form = document.forms['contact-form']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwScHu4cUVNVFnsYR7wzY_oh8EDB8xcjZ3ZH1uJqLzsFESFdEam_gu2VAHmEZCci-YX/exec';
+const form = document.forms['contact-form'];
+let enviado = false;
 
 form.addEventListener('submit', e => {
+  if (enviado) {
+    e.preventDefault();
+    return;
+  }
   
-  e.preventDefault()
+  enviado = true;
   
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  
-  .then(response => alert("Obrigado! Seu formulário foi enviado." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
-})
+  e.preventDefault();
+  fetch(scriptURL, {
+    method: 'POST',
+    body: new FormData(form)
+  })
+  .then(response => {
+    alert("Obrigado! Seu formulário foi enviado.");
+    form.reset(); // Limpa o formulário
+    enviado = false; // Reabilita o envio
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    enviado = false; // Reabilita o envio em caso de erro
+  });
+});
+
+
+
+
